31730de3696946ee2a7c229d49c28d3a0c71a2e2
gugakatsi <guga.katsiashvili@gmail.com>
2023-05-31 16:43:54 +0400

    Rename, Icon, Remove line breaks, css icon

--- src/index.js
+++ src/index.js
@@ -20,7 +20,9 @@ import { IconBrackets } from '@codexteam/icons';
 /**
  * Code Tool for the Editor.js allows to include code examples in your articles.
  */
-export default class CodeTool {
+export default class DomqlExecCode {
+  static DOMQLIcon = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24" height="24" viewBox="0 0 24 24"><path d="M30,17.1469762 C22.7235217,18.8081122 17.0638346,18.0431382 13.020939,14.8520542 C13.3375911,14.6386226 13.6544707,14.4191248 13.9722219,14.193096 L14.880722,13.5378499 C15.1984458,13.3111713 15.5088065,13.0929984 15.8124415,12.8831439 C20.397324,16.5246117 24.202813,17.0406935 30,17.1469762 Z M12.9433234,11.003894 C19.4699757,6.43726773 24.9985281,4.98314348 29.5243428,6.69568489 L30,6.87567077 C23.6663291,6.02376237 19.7604498,7.59509247 13.4610159,12.0021461 L12.5630519,12.6372666 C6.03923308,17.1877895 0.0117182515,18.849674 -5.50917623,17.5948151 L-6,17.4832544 C-0.699721992,18.2743969 6.00165134,15.9706747 12.5067346,11.3129462 Z M11.3765197,9.10771019 L11.0219638,9.35728644 C10.1898754,9.93625404 9.35848619,10.4719321 8.53148597,10.9632107 C4.45305756,7.94041617 0.89800993,7.31274926 -6,6.31730593 C1.52460747,5.44375324 7.31678071,6.373888 11.3765197,9.10771019 Z" /></svg>';
+
   /**
    * Notify core that read-only mode is supported
    *
@@ -136,7 +138,7 @@ export default class CodeTool {
    */
   save(codeWrapper) {
     return {
-      code: codeWrapper.querySelector('textarea').value,
+      code: codeWrapper.querySelector('textarea').value.replace(/\n/g, ''),
     };
   }
 
@@ -184,8 +186,8 @@ export default class CodeTool {
    */
   static get toolbox() {
     return {
-      icon: IconBrackets,
-      title: 'Code',
+      icon: this.DOMQLIcon,
+      title: 'DOMQL code',
     };
   }
 
@@ -196,7 +198,7 @@ export default class CodeTool {
    * @returns {string}
    */
   static get DEFAULT_PLACEHOLDER() {
-    return 'Enter a code';
+    return 'Enter a DOMQL code as object';
   }
 
   /**
